1fb81be9f4015c030389e22294e2335982ec95e1
Changzaoo <vinicinhos324@gmail.com>
2025-08-18 21:21:22 -0300

    ss

--- src/index.js
+++ src/index.js
@@ -9,14 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('SW registered:', registration);
-      }).catch(error => {
-        console.log('SW registration failed:', error);
-      });
-  });
-}
